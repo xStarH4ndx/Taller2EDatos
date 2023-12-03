c7d2f36b1f5b7c201e91adda9036823c429ae365
xStarH4ndx <146247223+xStarH4ndx@users.noreply.github.com>
2023-12-03 22:37:25 +0000

    asd

--- main.cpp
+++ main.cpp
@@ -64,26 +64,44 @@ void realizarMovimientoFacil(Nodo* nodo) {
 
 //LA MAQUINA REALIZARÁ MOVIMIENTOS SEGUIDOS (O-O-O-O)
 void realizarMovimientoMedio(Nodo* nodo) {
-    int mejorColumna= -1;
-    int mejorPuntuacion= INT_MIN;
-    for (int columna=0;columna<columnas;columna++) {
-        if (nodo->tablero[0][columna]!= " ") {
-            continue;
+    int mejorColumna = -1;
+    int mejorPuntuacion = INT_MIN;
+    // Evaluar cada columna para determinar el mejor movimiento
+    for (int columna = 0; columna < columnas; columna++) {
+        if (nodo->tablero[0][columna] != " ") {
+            continue;  // La columna está llena
         }
-        Nodo* hijo= new Nodo(*nodo);
-        hijo->realizarMovimiento(columna, 2);
-        int puntuacion= minimax(hijo, profundidadMaxima, false, INT_MIN, INT_MAX);
-        if (puntuacion>mejorPuntuacion) {
-            mejorPuntuacion= puntuacion;
-            mejorColumna= columna;
+        Nodo* hijo = new Nodo(*nodo);
+        hijo->realizarMovimiento(columna, 2);  // 2 representa al jugador máquina
+        //Calcular puntuación en función de la cantidad de fichas en la columna
+        int puntuacion = 0;
+        //Jugada vertical
+        for (int i = 0; i < filas; i++) {
+            if (hijo->tablero[i][columna] == "O") {
+                puntuacion++;
+            }
+        }
+        //Jugada horizontal
+        for (int j = 0; j < columnas; j++) {
+            if (hijo->tablero[filas - 1][j] == "O") {
+                puntuacion++;
+            }
+        }
+        //mayor puntuación
+        if (puntuacion > mejorPuntuacion) {
+            mejorPuntuacion = puntuacion;
+            mejorColumna = columna;
         }
         delete hijo;
     }
+
+    // Realizar el movimiento en la mejor columna
     nodo->realizarMovimiento(mejorColumna, 2);
-    cout<<"Maquina:"<<"\n"<<"<-------------->"<<endl;
+    cout << "Maquina:" << "\n" << "<-------------->" << endl;
     nodo->mostrarTablero();
-    cout<<"<-------------->"<<endl;
+    cout << "<-------------->" << endl;
 }
+
 
 //LA MAQUINA SIEMPRE GANARA O EMPATE
 void realizarMovimientoDificil(Nodo* nodo) {
